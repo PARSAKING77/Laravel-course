c782ef5233913dd823cda9d61c04bde0575e5ba1
PARSAKING77 <parsakingkp@gmail.com>
2025-01-08 13:01:25 +0330

    Edited resources folder

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,5 +1,5 @@
 import '../css/app.css';
-import './bootstrap.ts';  // Assuming you have this file
+import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -12,8 +12,8 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
-            `./Pages/${name}.vue`,  // Dynamically resolves your Vue page components
-            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),  // Dynamically import all Vue pages
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
--- resources/js/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-if (typeof window !== 'undefined') {
-    window.axios = axios;
-    window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-}
